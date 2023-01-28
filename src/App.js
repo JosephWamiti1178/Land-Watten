ef415b6b300d498a8057d831de8ccb7f02f3a948
Joseph <wamiti8711@gmail.com>
2023-01-27 19:08:28 -0500

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
 import ImageSlider from './components/Hero';
 import { SliderData } from './components/SliderData';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     < BrowserRouter>
-    <Header />
+    <Navbar />
     <ImageSlider slides={SliderData} />
     </BrowserRouter>
     
